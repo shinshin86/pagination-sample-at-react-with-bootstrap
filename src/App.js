fcd598a48e11a40a91ffcfbacedc809f0981bcc8
shinshin86 <beagles1986@gmail.com>
2019-12-28 07:42:54 +0900

    Add data fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getTestData } from './test-data';
 import './App.css';
 
 export default function() {
+  const [userList, setUsetList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await getTestData();
+      const data = await getTestData();
+      setUsetList(data)
+    }
+
+    fetchData();
+  }, [])
+  
   return (
     <div className="App">
       <h1>Pagination Sample</h1>
+      <div className='container'>
+        <ul className="list-group">
+          {userList.map(user => (
+            <li className="list-group-item" key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
